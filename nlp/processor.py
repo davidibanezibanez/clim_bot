82d14fa12e7fe6ffc4ae2798f64c271f720e9447
David Ibáñez <d.ibanez03@ufromail.cl>
2025-04-27 23:17:58 -0400

    Fix processor.py

--- nlp/processor.py
+++ nlp/processor.py
@@ -11,13 +11,14 @@ def get_intent(message):
     normalized_message = normalize_text(message)
     doc = nlp(normalized_message)
 
-    if any(token.lemma in ["clima", "tiempo"] for token in doc):
+    if any(token.lemma_ in ["clima", "tiempo"] for token in doc):
         return "weather"
     elif "unidad de fomento" in normalized_message or "uf" in normalized_message:
         return "uf"
-    elif any(token.lemma in ["dolar", "usd"] for token in doc):
+    elif any(token.lemma_ in ["dolar", "usd"] for token in doc):
         return "dollar"
     elif any(token.lemma_ in ["noticia", "noticias", "novedad"] for token in doc):
         return "news"
     else:
         return "unknown"
+    
